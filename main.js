a666c0972dc1f3a8903142b67203368e408cad77
Danis Tazhitdinov <tashitdinov1999@mail.ru>
2023-11-16 22:02:00 +0300

    das

--- main.js
+++ main.js
@@ -27,19 +27,50 @@ thirdPageButton.addEventListener('click', () => {
   fourthPage.classList.remove('fourth-page_disabled');
 });
 
-nailButtons.forEach((elem) => {
+nailButtons.forEach((elem, index) => {
   elem.addEventListener('click', () => {
-    fourthPageVideo.classList.add('fourth-page__video_active');
-    navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } })
+    switch (index) {
+      case 0:
+        nailButtons[0].src = './images/nail-circle-1-active.svg';
+        nailButtons[1].src = './images/nail-circle-2.svg';
+        nailButtons[2].src = './images/nail-circle-3.svg';
+        nails.forEach((nail, index) => {
+          nail.src = `./images/red-${index + 1}.png`;
+        });
+        break;
+      case 1:
+        nailButtons[1].src = './images/nail-circle-2-active.svg';
+        nailButtons[0].src = './images/nail-circle-1.svg';
+        nailButtons[2].src = './images/nail-circle-3.svg';
+        nails.forEach((nail, index) => {
+          nail.src = `./images/blue-${index + 1}.png`;
+        });
+        break;
+      case 2:
+        nailButtons[2].src = './images/nail-circle-3-active.svg';
+        nailButtons[1].src = './images/nail-circle-2.svg';
+        nailButtons[0].src = './images/nail-circle-1.svg';
+        nails.forEach((nail, index) => {
+          nail.src = `./images/gray-${index + 1}.png`;
+        });
+        break;
+    
+      default:
+        break;
+    }
+    if (!fourthPageVideo.className.includes('active')) {
+      navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } })
       .then((stream) => {
           fourthPageVideo.srcObject = stream;
+          fourthPageInfo.classList.add('fourth-page__info_disabled');
           nails.forEach((nail) => {
-            fourthPageInfo.classList.add('fourth-page__info_disabled');
             nail.classList.add('fourth-page__nail_active');
           });
       })
       .catch((error) => {
           console.error('Ошибка доступа к камере:', error);
       });
+    }
+    fourthPageVideo.classList.add('fourth-page__video_active');
   });
 });
