83c4017ce71a8dd4f9742a7a83ee326614e561d4
Danis Tazhitdinov <tashitdinov1999@mail.ru>
2023-11-20 12:18:55 +0300

    fdsfs

--- main.js
+++ main.js
@@ -8,15 +8,35 @@ const fourthPage = document.querySelector('.fourth-page');
 const fourthPageVideo = fourthPage.querySelector('.fourth-page__video');
 const fourthPageButton = fourthPage.querySelector('.fourth-page__button');
 const fourthPageInfo = fourthPage.querySelector('.fourth-page__info');
+const fourthPageTextChoose = fourthPage.querySelector('.fourth-page__text_choose');
+const fourthPageScreen = fourthPage.querySelector('.fourth-page__screen');
 const nails = fourthPage.querySelectorAll('.fourth-page__nail');
 const nailButtons = fourthPage.querySelectorAll('.fourth-page__circle');
-const onBtn = document.getElementById('on');
-const offBtn = document.getElementById('off');
+const finalPage = document.querySelector('.final-page');
+const finalPageIMG = finalPage.querySelector('.final-page__img');
+const finalPageSendButton = finalPage.querySelector('.final-page__send-button');
+const finalPageBackButton = finalPage.querySelector('.final-page__back-button');
+
+
+// additional constants for debug and help
+const hiddenIMG = document.querySelector('.hidden-image');
+const nailsSliced = document.querySelector('.nails-sliced');
+
+let detect = new MobileDetect(window.navigator.userAgent);
+
+console.log(detect.os());
+
+if (detect.os() === 'iOS') {
+  fourthPageButton.textContent = 'Продолжить'
+}
 
 
 firstPageButton.addEventListener('click', () => {
   firstPage.classList.add('first-page_disabled');
   secondPage.classList.remove('second-page_disabled');
+  if (detect.os() === 'iOS') {
+    startCamera();
+  }
 });
 
 secondPageButton.addEventListener('click', () => {
@@ -29,8 +49,89 @@ thirdPageButton.addEventListener('click', () => {
   fourthPage.classList.remove('fourth-page_disabled');
 });
 
+async function sendPhoto(assetElement) {
+  // Получение ссылки на изображение
+  const imageURL = assetElement.src;
+
+  // Загрузка изображения в бинарном формате
+  const response = await fetch(imageURL);
+  const imageBlob = await response.blob();
+
+  // Формируем объект FormData для отправки файла
+  const formData = new FormData();
+  formData.append('chat_id', userChatId);
+  formData.append('photo', imageBlob, 'photo.jpg');
+
+  // Формируем URL для отправки фотографии
+  const apiUrl = `https://api.telegram.org/bot${botToken}/sendPhoto`;
+
+  // Отправка фотографии на сервер Telegram
+  try {
+      const result = await fetch(apiUrl, {
+          method: 'POST',
+          body: formData,
+      });
+      const data = await result.json();
+      console.log(data);
+      if (data.ok) {
+          console.log('Фотография успешно отправлена в Telegram.');
+      } else {
+          console.error('Произошла ошибка при отправке фотографии.');
+      }
+  } catch (error) {
+      console.error('Ошибка:', error);
+  }
+}
+
+finalPageSendButton.addEventListener('click', () => {
+  sendPhoto(finalPageIMG);
+})
+
+fourthPageButton.addEventListener('click', () => {
+  if (detect.os() === 'iOS') {
+    startCamera();
+    fourthPageButton.textContent = 'Сохранить'
+  }
+  if (fourthPageInfo.className.includes('disabled')) {
+  
+
+    const tempCanvas = document.createElement('canvas');
+    const tempCtx = tempCanvas.getContext('2d');
+
+    const scaleFactor = 1;
+    tempCanvas.width = fourthPageVideo.clientWidth*scaleFactor;
+    tempCanvas.height = fourthPageVideo.clientHeight*scaleFactor;
+
+    const leftSmech = (fourthPageVideo.clientWidth - fourthPageScreen.clientWidth)/2;
+
+
+    tempCtx.drawImage(fourthPageVideo, 0, 0, tempCanvas.width, tempCanvas.height);
+
+    tempCtx.drawImage(
+          nailsSliced,
+          (nailsSliced.offsetLeft + leftSmech)*scaleFactor,
+          nailsSliced.offsetTop*scaleFactor,
+          nailsSliced.width*scaleFactor,
+          nailsSliced.height*scaleFactor
+        ); 
+
+    const dataURL = tempCanvas.toDataURL('image/png', 1.0);
+
+    hiddenIMG.width = fourthPageVideo.clientWidth;
+    hiddenIMG.height = fourthPageVideo.clientHeight;
+    hiddenIMG.src = dataURL;
+    finalPageIMG.src = dataURL;
+
+    fourthPage.classList.add('fourth-page_disabled');
+    finalPage.classList.remove('final-page_disabled');
+  }
+})
+
 nailButtons.forEach((elem, index) => {
   elem.addEventListener('click', () => {
+    if (detect.os() === 'iOS') {
+      stopCamera();
+    }
     switch (index) {
       case 0:
         nailButtons[0].src = './images/nail-circle-1-active.svg';
@@ -65,6 +166,7 @@ nailButtons.forEach((elem, index) => {
       .then((stream) => {
           fourthPageVideo.srcObject = stream;
           fourthPageInfo.classList.add('fourth-page__info_disabled');
+          fourthPageTextChoose.style.display = 'none';
           nails.forEach((nail) => {
             nail.classList.add('fourth-page__nail_active');
           });
@@ -81,10 +183,12 @@ function startCamera() {
   navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } })
       .then((stream) => {
           fourthPageVideo.srcObject = stream;
-          fourthPageInfo.classList.add('fourth-page__info_disabled');
-          nails.forEach((nail) => {
-            nail.classList.add('fourth-page__nail_active');
-          });
+          if (!fourthPage.className.includes('disabled')) {
+            fourthPageInfo.classList.add('fourth-page__info_disabled');
+            nails.forEach((nail) => {
+              nail.classList.add('fourth-page__nail_active');
+            });
+          }
       })
       .catch((error) => {
           console.error('Ошибка доступа к камере:', error);
@@ -100,10 +204,7 @@ function stopCamera() {
   fourthPageVideo.srcObject = null;
 }
 
-onBtn.addEventListener('click', () => {
-  startCamera();
-})
-
-offBtn.addEventListener('click', () => {
-  stopCamera();
-})
+finalPageBackButton.addEventListener('click', () => {
+  finalPage.classList.add('final-page_disabled');
+  fourthPage.classList.remove('fourth-page_disabled');
+});
